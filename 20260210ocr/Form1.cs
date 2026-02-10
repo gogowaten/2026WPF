896a712ebdfb3ac78b09a225efdf1f9298a275b4
waten <gogowaten@yahoo.co.jp>
2026-02-10 14:57:58 +0900

    202602101457

---
+++ 20260210ocr/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace _20260210ocr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                var img = (Bitmap)Clipboard.GetImage();
+
+                using (var tesseract = new TesseractEngine(@"./", "jpn"))
+                {
+                    Pix pix = PixConverter.ToPix(img);
+                    Page page = tesseract.Process(pix);
+                    textBox1.Text = page.GetText().Replace("\n", "\r\n");
+                }
+            }
+
+        }
+    }
+}
