4ed3584359e730c3b429f75af399ece40c733912
waten <gogowaten@yahoo.co.jp>
2026-02-23 14:21:04 +0900

    202602231420

---
+++ 20260223/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace _20260223
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<Item> RootItems { get; } = [];
+
+        public MainViewModel()
+        {
+            var root = new Item("メインテーマ", 100, 100);
+
+            var child1 = new Item("アイデア A", 150, -50);
+            child1.Children.Add(new Item("詳細 A-1", 100, 0));
+
+            var child2 = new Item("アイデア B", 150, 90);
+
+            root.Children.Add(child1);
+            root.Children.Add(child2);
+
+            RootItems.Add(root);
+        }
+    }
+
+}
---
+++ 20260223/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260223
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public ObservableCollection<Item> ItemData { get; set; } = [];
+        public Item MyItem { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //MyItem = new("Root", 20, 10);
+            //MyItem.Children.Add(new("111", 100, 10));
+            //MyItem.Children.Add(new("222", 100, 50));
+            //DataContext = MyItem;
+
+        }
+    }
+}
