cc55d3f35c14e50e10b14898431d14ec6c19070d
waten <gogowaten@yahoo.co.jp>
2026-03-06 16:46:01 +0900

    202603061645

---
+++ 20260306_DataTemplateBindingData/Data.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace _20260306_DataTemplateBindingData
+{
+    public partial class EllipseData : ShapeData { public EllipseData() { Width = 50; Height = 50; } }
+
+    public partial class RectangleData : ShapeData
+    {
+        public RectangleData()
+        {
+            Width = 100; Height = 50; X = 200; Y = 0;
+            Fill = new SolidColorBrush(Color.FromArgb(50, 0, 200, 255));
+        }
+    }
+
+    public abstract partial class ShapeData : Data
+    {
+        [ObservableProperty] private Brush _fill = new SolidColorBrush(Color.FromArgb(50, 255, 0, 0));
+    }
+
+
+    public abstract partial class Data : ObservableObject
+    {
+        [ObservableProperty] private double _width;
+        [ObservableProperty] private double _height;
+        [ObservableProperty] private double _x;
+        [ObservableProperty] private double _y;
+        //[ObservableProperty] private string _name = string.Empty;
+        //[ObservableProperty] bool _isSelected = false;
+    }
+}
---
+++ 20260306_DataTemplateBindingData/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260306_DataTemplateBindingData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public RectangleData MyRectangleData { get; set; } = new RectangleData();
+        public EllipseData MyEllipseData { get; set; } = new EllipseData();
+        public EllipseData My金丸Data { get; set; } = new EllipseData() { X = 150, Y = 50, Fill = Brushes.Gold };
+        public EllipseData Myオリーブ丸Data { get; set; } = new EllipseData() { X = 100, Y = 150, Fill = Brushes.Olive };
+        public EllipseData My緑丸Data { get; set; } = new EllipseData() { X = 100, Y = 100, Fill = Brushes.MediumAquamarine };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+    }
+}
