a0e083e55ddcb6c6a039a418967389d8b02271a5
waten <gogowaten@yahoo.co.jp>
2026-03-06 13:44:15 +0900

    202603061344

---
+++ 20260306_DaraTemplateRectangle/Data.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20260306_DaraTemplateRectangle
+{
+    public abstract partial class Data : ObservableObject
+    {
+        [ObservableProperty] private double _width;
+        [ObservableProperty] private double _height;
+        //[ObservableProperty] private double _x;
+        //[ObservableProperty] private double _y;
+        [ObservableProperty] private string _name = string.Empty;
+        //[ObservableProperty] bool _isSelected = false;
+    }
+
+    public partial class RectangleData : Data
+    {
+        
+        public RectangleData(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+
+}
---
+++ 20260306_DaraTemplateRectangle/ViewModels.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20260306_DaraTemplateRectangle
+{
+    public partial class RectangleVM : ObservableObject
+    {
+        public RectangleVM()
+        {
+            this._data = new(50, 150);
+        }
+
+        [ObservableProperty] private RectangleData _data;
+
+
+    }
+
+    internal class ViewModels
+    {
+    }
+}
