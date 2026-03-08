7f3e006d821485e626778aa61b6870c3bac1cb72
waten <gogowaten@yahoo.co.jp>
2026-03-08 19:52:31 +0900

    202603081952

---
+++ 20260308_CustomContainer2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260308_CustomContainer2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //ListBox lb = new();
+            //ItemCollection ic = lb.Items;
+            //lb.SelectionMode = SelectionMode.Multiple;
+
+            //ItemsControl ictrl = new();
+            //ItemCollection icci = ictrl.Items;
+        }
+    }
+}
---
+++ 20260308_CustomContainer2/MySelector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Runtime.CompilerServices;
+
+namespace _20260308_CustomContainer2
+{
+    public class MySelector : ItemsControl
+    {
+
+        public object SelectedItem
+        {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register(nameof(SelectedItem), typeof(object), typeof(MySelector), new FrameworkPropertyMetadata(null, OnSelectedItemChanged));
+
+        private static void OnSelectedItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MySelector selector && e.NewValue is UIElement element)
+            {
+                selector.UpdateVisualSelection(element);
+            }
+        }
+
+        private void UpdateVisualSelection(object selectedItem)
+        {
+            foreach (var item in Items)
+            {
+                if (ItemContainerGenerator.ContainerFromItem(item) is MySelectorItem container)
+                {
+                    container.IsSelected = (item == selectedItem);
+                }
+            }
+        }
+
+
+        // --- 1. アイテムが既にコンテナかチェック ---
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            //return base.IsItemItsOwnContainerOverride(item);
+            return item is MySelectorItem;
+        }
+
+        // --- 2. 新しいコンテナ（器）を作成 ---
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            //return base.GetContainerForItemOverride();
+            return new MySelectorItem();
+        }
+
+
+
+
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            base.PrepareContainerForItemOverride(element, item);
+
+            if (element is MySelectorItem container)
+            {
+                container.IsSelected = (item == SelectedItem);
+                container.PreviewMouseDown += OnContainerMouseDown;
+            }
+        }
+
+        private void OnContainerMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (sender is MySelectorItem container)
+            {
+                SelectedItem = ItemContainerGenerator.ItemFromContainer(container);
+            }
+        }
+
+        // コンテナの後処理、購読解除と状態のリセット
+        protected override void ClearContainerForItemOverride(DependencyObject element, object item)
+        {
+            if (element is MySelectorItem container)
+            {
+                container.PreviewMouseDown -= OnContainerMouseDown;
+                container.IsSelected = false;
+            }
+
+            base.ClearContainerForItemOverride(element, item);
+        }
+    }
+}
