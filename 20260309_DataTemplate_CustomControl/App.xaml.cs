4136f4e7924729592a928762be28c31cefd300ce
waten <gogowaten@yahoo.co.jp>
2026-03-09 14:57:39 +0900

    202603091457

---
+++ 20260309_DataTemplate_CustomControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20260309_DataTemplate_CustomControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 20260309_DataTemplate_CustomControl/CustomControl1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260309_DataTemplate_CustomControl
+{
+
+    public class AAA : ItemsControl
+    {
+        static AAA()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AAA), new FrameworkPropertyMetadata(typeof(AAA)));
+        }
+        public AAA()
+        {
+
+        }
+
+    }
+
+
+
+}
