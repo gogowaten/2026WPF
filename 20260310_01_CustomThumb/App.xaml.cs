56a7dca2937d509cff2bb273731588ccb949ece2
waten <gogowaten@yahoo.co.jp>
2026-03-10 13:16:05 +0900

    202603101315

---
+++ 20260310_01_CustomThumb/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20260310_01_CustomThumb
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
