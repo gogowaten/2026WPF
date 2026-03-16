1d1377afa67f2458e961eea7e8946a62d27c2acb
waten <gogowaten@yahoo.co.jp>
2026-03-17 00:54:36 +0900

    202603170054

--- 20260316_TextBlockSizeを添付プロパティで/SizeObserver.cs
+++ 20260316_TextBlockSizeを添付プロパティで/SizeObserver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 
+// サイズ変更時にActualWidthとActualHeightを自動取得したいときに使う添付プロパティ
 namespace _20260316_TextBlockSizeを添付プロパティで
 {
     /// <summary>
@@ -14,40 +15,51 @@ namespace _20260316_TextBlockSizeを添付プロパティで
     /// これは、カスタムイベントハンドラーを使用せずに、XAML でサイズ変更に反応したり、バインドしたりする必要がある場合に役立ちます。</remarks>
     public static class SizeObserver
     {
-        // true で監視が有効状態になり、ActualWidthとActualHeightが自動更新される
+        // true で監視のオンオフ切り替え
         public static readonly DependencyProperty ObserveProperty =
             DependencyProperty.RegisterAttached("Observe", typeof(bool), typeof(SizeObserver), new FrameworkPropertyMetadata(OnObserveChanged));
         public static bool GetObserve(DependencyObject obj) => (bool)obj.GetValue(ObserveProperty);
         public static void SetObserve(DependencyObject obj, bool value) => obj.SetValue(ObserveProperty, value);
 
 
-        // ActualWidthとの連携になる
+        // ActualWidth用
         public static readonly DependencyProperty ObserveWidthProperty =
             DependencyProperty.RegisterAttached("ObserveWidth", typeof(double), typeof(SizeObserver), new PropertyMetadata(0.0));
-        public static double GetObserveWidth(DependencyObject obj) => (double)obj.GetValue(ObserveWidthProperty);
+        //public static double GetObserveWidth(DependencyObject obj) => (double)obj.GetValue(ObserveWidthProperty);
         public static void SetObserveWidth(DependencyObject obj, double value) => obj.SetValue(ObserveWidthProperty, value);
 
         // ActualHeight用
         public static readonly DependencyProperty ObserveHeightProperty =
             DependencyProperty.RegisterAttached("ObserveHeight", typeof(double), typeof(SizeObserver), new PropertyMetadata(0.0));
-        public static double GetObserveHeight(DependencyObject obj) => (double)obj.GetValue(ObserveHeightProperty);
+        //public static double GetObserveHeight(DependencyObject obj) => (double)obj.GetValue(ObserveHeightProperty);
         public static void SetObserveHeight(DependencyObject obj, double value) => obj.SetValue(ObserveHeightProperty, value);
 
 
 
-        // 値変更時の処理、監視が有効状態なら対象要素のサイズ変更があった場合に
+        // 監視オンでSizeChangedイベント購読、サイズ変更時にそれぞれのプロパティを書き換える
         // 要素のActualWidthとActualHeightをsetする
         private static void OnObserveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is FrameworkElement fe && (bool)e.NewValue)
+            if (d is FrameworkElement fe)
             {
-                fe.SizeChanged += (s, args) =>
+                if ((bool)e.NewValue)
                 {
-                    SetObserveWidth(fe, fe.ActualWidth);
-                    SetObserveHeight(fe, fe.ActualHeight);
-                };
+                    fe.SizeChanged += Fe_SizeChanged;
+                }
+                else
+                {
+                    fe.SizeChanged -= Fe_SizeChanged;
+                }
             }
         }
 
+        private static void Fe_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (sender is FrameworkElement fe)
+            {
+                SetObserveWidth(fe, fe.ActualWidth);
+                SetObserveHeight(fe, fe.ActualHeight);
+            }
+        }
     }
 }
