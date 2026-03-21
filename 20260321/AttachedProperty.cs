77d741585f860d73a452c1f0abf3d80faaf2889a
waten <gogowaten@yahoo.co.jp>
2026-03-22 01:59:49 +0900

    202603220159

---
+++ 20260321/AttachedProperty.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace _20260321
+{
+
+    // TextBlockなどWidthとHeightがNaNの要素からサイズを取得するときに使う添付プロパティ
+    // 取得する値は、たぶんActualHeight、ActualWidth
+    /*                   local:SizeObserver.Observe="True"
+                         local:SizeObserver.ObservedWidth="{Binding Width, Mode=OneWayToSource}"
+                         local:SizeObserver.ObservedHeight="{Binding Height, Mode=OneWayToSource}"/>
+    みたいに使う、BindingModeはOneWayToSourceで取得用になる
+     */
+    public static class SizeObserver
+    {
+        // サイズ変更監視のオンオフ切り替え
+        public static readonly DependencyProperty ObserveProperty =
+            DependencyProperty.RegisterAttached("Observe", typeof(bool), typeof(SizeObserver), new FrameworkPropertyMetadata(OnObserveChanged));
+        public static bool GetObserve(DependencyObject obj) => (bool)obj.GetValue(ObserveProperty);
+        public static void SetObserve(DependencyObject obj, bool value) => obj.SetValue(ObserveProperty, value);
+
+        // サイズ変更イベントの購読と処理の付け外し
+        private static void OnObserveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FrameworkElement fe)
+            {
+                if ((bool)e.NewValue)
+                {
+                    fe.SizeChanged += Fe_SizeChanged;
+                    //SetObservedWidth(fe, fe.ActualWidth);// これだと初回読み込み時のサイズが常に0になってしまう
+
+                    //初回のみLoadedイベントをフックして、レンダリング後のサイズを取得する
+                    fe.Loaded += Fe_Loaded;
+                }
+                else
+                {
+                    fe.SizeChanged -= Fe_SizeChanged;
+                }
+            }
+        }
+
+        private static void Fe_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement fe)
+            {
+                // 読み込みが完了したので、この時点のActualWidthを反映
+                SetObservedWidth(fe, fe.ActualWidth);
+                SetObservedHeight(fe, fe.ActualHeight);
+                // 一度取得すれば用済みなので、イベントを外してメモリリークを防止
+                fe.Loaded -= Fe_Loaded;
+            }
+        }
+
+
+        // サイズ変更時の処理、新たなサイズをObservedWidthとHeightそれぞれに記録
+        private static void Fe_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width != 0 && e.NewSize.Height != 0)
+            {
+                if (sender is FrameworkElement fe)
+                {
+                    SetObservedWidth(fe, e.NewSize.Width);
+                    SetObservedHeight(fe, e.NewSize.Height);
+                    //SetObservedHeight(fe, fe.DesiredSize.Height); // これで問題ない？
+
+                }
+            }
+        }
+
+        // Width記録用
+        public static readonly DependencyProperty ObservedWidthProperty =
+            DependencyProperty.RegisterAttached("ObservedWidth", typeof(double), typeof(SizeObserver), new PropertyMetadata(0.0));
+        public static double GetObservedWidth(DependencyObject obj) => (double)obj.GetValue(ObservedWidthProperty);
+        public static void SetObservedWidth(DependencyObject obj, double value) => obj.SetValue(ObservedWidthProperty, value);
+
+        // Height記録用
+        public static readonly DependencyProperty ObservedHeightProperty =
+            DependencyProperty.RegisterAttached("ObservedHeight", typeof(double), typeof(SizeObserver), new PropertyMetadata(0.0));
+        public static double GetObservedHeight(DependencyObject obj) => (double)obj.GetValue(ObservedHeightProperty);
+        public static void SetObservedHeight(DependencyObject obj, double value) => obj.SetValue(ObservedHeightProperty, value);
+
+        //// --- 実行したいコマンド用のプロパティ ---
+        //public static readonly DependencyProperty WidthChangedCommandProperty =
+        //    DependencyProperty.RegisterAttached("WidthChangedCommand", typeof(ICommand), typeof(SizeObserver), new PropertyMetadata(null));
+        //public static ICommand GetWidthChangedCommand(DependencyObject obj) => (ICommand)obj.GetValue(WidthChangedCommandProperty);
+        //public static void SetWidthChangedCommand(DependencyObject obj, ICommand value) => obj.SetValue(WidthChangedCommandProperty, value);
+
+    }
+
+    internal class AttachedProperty
+    {
+    }
+}
---
+++ 20260321/CustomControl1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260321
+{
+
+    [ContentProperty(nameof(MyContent))]
+    public class CustomThumb : Thumb
+    {
+        public override string ToString()
+        {
+            //return base.ToString();
+            return MyData.Name;
+        }
+
+        static CustomThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomThumb), new FrameworkPropertyMetadata(typeof(CustomThumb)));
+        }
+
+
+        #region 依存関係プロパティ
+
+
+        public Data MyData
+        {
+            get { return (Data)GetValue(MyDataProperty); }
+            set { SetValue(MyDataProperty, value); }
+        }
+        public static readonly DependencyProperty MyDataProperty =
+            DependencyProperty.Register(nameof(MyData), typeof(Data), typeof(CustomThumb), new PropertyMetadata(null));
+
+
+
+        public FrameworkElement MyContent
+        {
+            get { return (FrameworkElement)GetValue(MyContentProperty); }
+            set { SetValue(MyContentProperty, value); }
+        }
+        public static readonly DependencyProperty MyContentProperty =
+            DependencyProperty.Register(nameof(MyContent), typeof(FrameworkElement), typeof(CustomThumb), new PropertyMetadata(null));
+        #endregion 依存関係プロパティ
+
+        //public bool MyIsSelected
+        //{
+        //    get { return (bool)GetValue(MyIsSelectedProperty); }
+        //    set { SetValue(MyIsSelectedProperty, value); }
+        //}
+        //public static readonly DependencyProperty MyIsSelectedProperty =
+        //    DependencyProperty.Register(nameof(MyIsSelected), typeof(bool), typeof(CustomThumb), new PropertyMetadata(false));
+
+
+
+        public CustomThumb()
+        {
+            //this.DataContext = this;
+
+            // 以下はXAMLのほうで処理するように変更した
+            // TextBlockなどサイズが確定していない要素を
+            // まっさらなRootに追加した直後にRootのサイズを決定するのに使う
+            //Loaded += CustomThumb_Loaded; // これは動くけど、DataTemplateからだとクリックのたびに実行される
+            //Initialized += CustomThumb_Initialized;// こっちだとまだ描画されていない感じ
+            DragDelta += CustomThumb_DragDelta;
+            DragCompleted += CustomThumb_DragCompleted;
+        }
+
+        private void CustomThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            MyData.RootData?.UpdateSize();
+        }
+
+        private void CustomThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            MyData.X += e.HorizontalChange;
+            MyData.Y += e.VerticalChange;
+        }
+
+        //// 起動時
+        //private void CustomThumb_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    // TextBlockなどのサイズがNaNの要素が追加された時用
+        //    // Dataが追加された時点で親要素のサイズ計測がされるけど、これらの要素はその時点でのサイズは0で
+        //    // 正しいサイズが設定されるのは描画後で、それがここなので、ここで親要素のサイズ計測
+
+
+        //    if (MyData is TextData text && text.RootData is RootData root)
+        //    {
+        //        if (text.Width != 0 && root.DataList.Count == 1)
+        //        {
+        //            root.UpdateSize();
+        //        }
+
+        //    }
+        //    // 起動時に1回だけ実行されればいいので、ここで解除
+        //    // とは言ってもDateTemplateで表示しているとクリックのたびに再作成している？から意味ないかも？
+        //    Loaded -= CustomThumb_Loaded;
+        //}
+
+
+        //protected override bool HandlesScrolling
+        //{
+        //    get
+        //    {
+        //        //return base.HandlesScrolling;
+        //        return true;
+        //    }
+        //}
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public class AAAItemsCtrl : ItemsControl
+    {
+
+        public RootData MyRootData
+        {
+            get { return (RootData)GetValue(MyRootDataProperty); }
+            set { SetValue(MyRootDataProperty, value); }
+        }
+        public static readonly DependencyProperty MyRootDataProperty =
+            DependencyProperty.Register(nameof(MyRootData), typeof(RootData), typeof(AAAItemsCtrl), new PropertyMetadata(null));
+
+
+
+        static AAAItemsCtrl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(AAAItemsCtrl), new FrameworkPropertyMetadata(typeof(AAAItemsCtrl)));
+        }
+        public AAAItemsCtrl()
+        {
+          
+        }
+
+
+    }
+
+
+}
