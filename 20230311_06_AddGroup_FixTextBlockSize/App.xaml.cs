cdcea8354897ee07ab735890e77a0d4b892fc4b2
waten <gogowaten@yahoo.co.jp>
2026-03-21 21:19:44 +0900

    202603212119

---
+++ 20230311_06_AddGroup_FixTextBlockSize/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20230311_06_AddGroup_FixTextBlockSize
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
