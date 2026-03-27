60cb0d9db4b8b0c835077266a5cdc56b5345e573
waten <gogowaten@yahoo.co.jp>
2026-03-27 18:26:11 +0900

    202603271826

---
+++ 20260327/GeoShape.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _20260327
+{
+    public class GeoShape : Shape
+    {
+        //private Vector _lastOffset;// 描画位置オフセット用
+        private Geometry? _cachedGeometry;
+        private Rect _lastRawBounds;
+        //public bool IsOffset = false;
+
+        #region 依存関係プロパティ
+
+        // オフセット無しの描画Bounds
+        public Rect OriginRenderBounds
+        {
+            get { return (Rect)GetValue(OriginRenderBoundsProperty); }
+            set { SetValue(OriginRenderBoundsProperty, value); }
+        }
+        public static readonly DependencyProperty OriginRenderBoundsProperty =
+            DependencyProperty.Register(nameof(OriginRenderBounds), typeof(Rect), typeof(GeoShape), new PropertyMetadata(new Rect()));
+
+
+        // オフセットするかどうかのフラグ
+        public bool IsOffset
+        {
+            get { return (bool)GetValue(IsOffsetProperty); }
+            set { SetValue(IsOffsetProperty, value); }
+        }
+        public static readonly DependencyProperty IsOffsetProperty =
+            DependencyProperty.Register(nameof(IsOffset), typeof(bool), typeof(GeoShape), new PropertyMetadata(false));
+
+        public Pen StrokePen
+        {
+            get { return (Pen)GetValue(StrokePenProperty); }
+            set { SetValue(StrokePenProperty, value); }
+        }
+        public static readonly DependencyProperty StrokePenProperty =
+            DependencyProperty.Register(nameof(StrokePen), typeof(Pen), typeof(GeoShape), new FrameworkPropertyMetadata(null, OnStrokePenChanged));
+
+        private static void OnStrokePenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GeoShape GeoShape)
+            {
+                GeoShape.UpdateRenderBounds();
+            }
+        }
+
+        // 実際の描画のBounds
+        public Rect RenderBounds
+        {
+            get { return (Rect)GetValue(RenderBoundsProperty); }
+            set { SetValue(RenderBoundsProperty, value); }
+        }
+        public static readonly DependencyProperty RenderBoundsProperty =
+            DependencyProperty.Register(nameof(RenderBounds), typeof(Rect), typeof(GeoShape), new FrameworkPropertyMetadata(new Rect(0, 0, 0, 0), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public PointCollection Points
+        {
+            get { return (PointCollection)GetValue(PointsProperty); }
+            set { SetValue(PointsProperty, value); }
+        }
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.Register(nameof(Points), typeof(PointCollection), typeof(GeoShape),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender, OnPointsChanged));
+
+
+        //private static void OnPointsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        //{
+        //    var shape = (GeoShape)d;
+
+        //    // 古いコレクションのイベントを解除
+        //    if (e.OldValue is PointCollection oldPC && !oldPC.IsFrozen)
+        //    {
+        //        oldPC.Changed -= shape.OnPointsCollectionChanged;
+        //    }
+        //    // 新しいコレクションのイベントを購読
+        //    if (e.NewValue is PointCollection newPC && !newPC.IsFrozen)
+        //    {
+        //        newPC.Changed += shape.OnPointsCollectionChanged;
+        //    }
+
+        //    //shape.UpdateRenderBounds();
+        //}
+
+        // OnPointsChangedでの処理はこれで十分？
+        private static void OnPointsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var shape = (GeoShape)d;
+            shape.UpdateRenderBounds();
+        }
+
+        // 既存の OnPointsChanged などを利用してGeometryキャッシュを破棄する
+        private void OnPointsCollectionChanged(object? sender, EventArgs e)
+        {
+            _cachedGeometry = null; // キャッシュクリア
+            // コレクションの中身が変わったときに再描画とサイズ更新を強制
+            InvalidateVisual();
+            UpdateRenderBounds();
+        }
+        #endregion 依存関係プロパティ
+
+
+        public GeoShape()
+        {
+            MultiBinding mb = new() { Converter = new ConvStrokePen() };
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeThicknessProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeMiterLimitProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeEndLineCapProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeStartLineCapProperty) });
+            mb.Bindings.Add(new Binding() { Source = this, Path = new PropertyPath(StrokeLineJoinProperty) });
+            SetBinding(StrokePenProperty, mb);
+
+            Loaded += GeoShape_Loaded; // 起動直後に表示されないときはあったほうが良い？必要ないかも
+        }
+
+        private void GeoShape_Loaded(object sender, RoutedEventArgs e)
+        {
+            UpdateRenderBounds();
+            InvalidateVisual(); // 再描画を強制
+        }
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                // キャッシュが在ればそれを返して終わる
+                if (_cachedGeometry is not null) { return _cachedGeometry; }
+
+                if (Points is null || Points.Count == 0) { return Geometry.Empty; }
+
+                var geo = MakeBezierPathGeometry();
+
+                // 変形前のBoundsを記録
+                OriginRenderBounds = geo.GetRenderBounds(StrokePen);
+
+                // オフセット有効なら、変形後のGeometryをキャッシュ
+                // 無効ならそのままのGeometryをキャッシュ
+                if (IsOffset)
+                {
+                    // 描画位置が(0,0)になるようにオフセットしたGeometryを作成して返す
+                    var transformedGeo = geo.Clone();
+                    transformedGeo.Transform = new TranslateTransform(-OriginRenderBounds.X, -OriginRenderBounds.Y);
+                    transformedGeo.Freeze();
+                    _cachedGeometry = transformedGeo;
+                }
+                else
+                {
+                    // オフセットなしのGeometryを返す
+                    geo.Freeze();
+                    _cachedGeometry = geo;
+                }
+
+                RenderBounds = _cachedGeometry.GetRenderBounds(StrokePen);
+                return _cachedGeometry;
+            }
+
+        }
+
+        private PathGeometry MakeBezierPathGeometry()
+        {
+            var figure = new PathFigure() { StartPoint = Points[0] };
+            var segment = new PolyBezierSegment();
+            for (int i = 1; i < Points.Count; i++)
+            {
+                segment.Points.Add(Points[i]);
+            }
+            figure.Segments.Add(segment);
+            var geo = new PathGeometry([figure]);
+            return geo;
+        }
+
+
+        private void DrawBezier(StreamGeometryContext context, Point begin, bool isFill, bool isClose, bool isSmoothJoin)
+        {
+            context.BeginFigure(begin, isFill, isClose);
+            List<Point> bezier = Points.ToList();
+            //var bezier = Points.Clone();
+            bezier.RemoveAt(0);
+
+            context.PolyBezierTo(bezier, true, isSmoothJoin);
+        }
+
+        // オフセット版
+        public void UpdateRenderBounds()
+        {
+            _cachedGeometry = null; // 強制再計算
+
+            if (Points is null || Points.Count == 0 || StrokePen == null) { return; }
+
+            // 見た目上のBoundsをpenを使って取得
+            Rect bounds = DefiningGeometry.GetRenderBounds(StrokePen);
+
+            // なにも描画するものがない（サイズが0）場合は更新しない
+            if (bounds.Width == 0 || bounds.Height == 0) return;
+
+            RenderBounds = bounds;
+
+            //if (DataContext is GeoShapeData data)
+            //{
+            //    //data.Width = _lastRawBounds.Width;
+            //    //data.Height = _lastRawBounds.Height;
+            //    //data.Left = _lastRawBounds.X;
+            //    //data.Top = _lastRawBounds.Y;
+            //    var neko = RenderBounds;
+            //    var last = _lastRawBounds;
+            //    data.Width = RenderBounds.Width;
+            //    data.Height = RenderBounds.Height;
+            //    data.Left = RenderBounds.Left;
+            //    data.Top = RenderBounds.Top;
+            //}
+        }
+
+
+    }
+}
---
+++ 20260327/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260327
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyShape.StrokeThickness = 30;
+            MyShape.Stroke = Brushes.MediumAquamarine;
+
+            MyShape.Points.Add(new Point(50, 150));
+            MyShape.Points.Add(new Point(150, 100));
+            MyShape.Points.Add(new Point(250, 250));
+
+            //MyShape.Points[0] = new Point(-100,50);
+            MyShape.Points[0] = new Point(-10, 50);
+
+        }
+
+        private void MyShape_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var neko = 0;
+        }
+    }
+}
