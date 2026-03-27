cc1ade1f1bba9193706c197aede54d121d03dff4
waten <gogowaten@yahoo.co.jp>
2026-03-27 22:46:55 +0900

    202603272246

---
+++ 20260327_01_OffsetGeoShape/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _20260327_01_OffsetGeoShape
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 20260327_01_OffsetGeoShape/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260327_01_OffsetGeoShape
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MyShape.StrokeThickness = 30;
+            //MyShape.Stroke = Brushes.MediumAquamarine;
+
+            MyShape.Points.Add(new Point(50, 150));
+            MyShape.Points.Add(new Point(150, 100));
+            MyShape.Points.Add(new Point(250, 250));
+
+            //先頭座標変更
+            MyShape.Points[0] = new Point(-10, 50);
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Pointsクリア
+            MyShape.Points.Clear();
+
+        }
+    }
+}
