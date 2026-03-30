18b5234f4ac13e82532738b15c1cba700f43b78d
waten <gogowaten@yahoo.co.jp>
2026-03-30 21:24:43 +0900

    202603302124

--- 20260330/MainWindow.xaml.cs
+++ 20260330/MainWindow.xaml.cs
@@ -24,10 +24,7 @@ namespace _20260330
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            if(AdornerLayer.GetAdornerLayer(TargetRect) is AdornerLayer layer)
-            {
-                layer.Add(new ResizeAdorner(TargetRect));
-            }
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -37,6 +34,60 @@ namespace _20260330
             {
                 layer.Add(new ResizeAdorner(MyCanvas));
             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddResizeAdorner(TargetRect);
+        }
+
+        /// <summary>
+        /// 指定された UI 要素に、サイズ変更アドーナがまだ存在しない場合に、追加します。
+        /// </summary>
+        /// <remarks>このメソッドは、指定された要素にサイズ変更アドーナが既に存在するかどうかを確認してから、新しいアドーナを追加します。
+        /// 要素はビジュアルツリーの一部であり、関連付けられたアドーナレイヤーを持っている必要があります。
+        /// </remarks>
+        /// <param name="element">サイズ変更アドーナを追加する UI 要素。null は指定できません。</param>
+        public void AddResizeAdorner(UIElement element)
+        {
+            if (AdornerLayer.GetAdornerLayer(element) is AdornerLayer layer)
+            {
+                var adorners = layer.GetAdorners(element);
+                if (adorners is null || adorners.Length == 0)
+                {
+                    layer.Add(new ResizeAdorner(element));
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            RemoveResizeAdorner(TargetRect);
+        }
+
+        /// <summary>
+        /// 指定された UI 要素から、すべてのアドナーを削除します。
+        /// </summary>
+        /// <remarks>このメソッドは、指定された要素に関連付けられたすべてのアドナーを検索して削除します。
+        /// アドナーが見つからない場合は、false を返します。操作を成功させるには、要素がビジュアルツリーの一部である必要があります。
+        /// </remarks>
+        /// <param name="element">アドナーを削除する UI 要素。null は指定できません。</param>
+        /// <returns>削除されたAdornerの個数を返す</returns>
+        public int RemoveResizeAdorner(UIElement element)
+        {
+            int result = 0;
+            if (AdornerLayer.GetAdornerLayer(element) is AdornerLayer layer)
+            {
+                if (layer.GetAdorners(element) is Adorner[] ados)
+                {
+                    foreach (Adorner item in ados)
+                    {
+                        layer.Remove(item);
+                        result++;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
