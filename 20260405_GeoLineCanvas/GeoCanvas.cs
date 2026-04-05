03f094a6fffbb9a4de54c4481fa0998d04a81d1a
waten <gogowaten@yahoo.co.jp>
2026-04-05 16:54:11 +0900

    202604051654

---
+++ 20260405_GeoLineCanvas/GeoCanvas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _20260405_GeoLineCanvas
+{
+    public class GeoCanvas : Canvas
+    {
+        public CustomThumb MyCustomThumb { get; set; }
+
+        public GeoLineData MyGeoData
+        {
+            get { return (GeoLineData)GetValue(MyGeoDataProperty); }
+            set { SetValue(MyGeoDataProperty, value); }
+        }
+        public static readonly DependencyProperty MyGeoDataProperty =
+            DependencyProperty.Register(nameof(MyGeoData), typeof(GeoLineData), typeof(GeoCanvas), new PropertyMetadata(null));
+
+        public GeoCanvas()
+        {
+            MyCustomThumb = new CustomThumb();
+            Children.Add(MyCustomThumb);
+
+            Loaded += GeoCanvas_Loaded;
+        }
+
+        private void GeoCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is GeoLineData data)
+            {
+                MyGeoData = data;
+                MyCustomThumb.MyData = MyGeoData;
+            }
+        }
+
+
+    }
+}
---
+++ 20260405_GeoLineCanvas/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260405_GeoLineCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GeoLineData MyData { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyData = TestGeoLineData();
+            DataContext = this;
+        }
+
+        private GeoLineData TestGeoLineData()
+        {
+            GeoLineData data = new()
+            {
+                Background = Brushes.Lavender,
+                //Fill = Brushes.DeepSkyBlue;
+                Stroke = Brushes.Orchid,
+                StrokeThickness = 20,
+                Points = [(new Point(50, 70)),
+                        (new Point(100, 150)),
+                        (new Point(50, 250)),
+                        (new Point(50, 200)),],
+                MiterLimit = 10,
+            };
+            return data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyData.Points.Count > 0)
+            {
+                MyData.Points[1] = new Point(-10, 10);
+            }
+        }
+
+        private void Button_Click_ChangeOffset(object sender, RoutedEventArgs e)
+        {
+            MyData.IsOffset = !MyData.IsOffset;
+        }
+
+        private void Button_Click_AddPoints(object sender, RoutedEventArgs e)
+        {
+            MyData.Points.Add(new Point(150, 200));
+            MyData.Points.Add(new Point(30, 100));
+            MyData.Points.Add(new Point(50, 200));
+        }
+
+        private void Button_Click_Check(object sender, RoutedEventArgs e)
+        {
+            var myData = MyData;
+            var elementDC = MyElement.DataContext;
+            //var elementMyData = MyElement.MyData;
+        }
+
+        private void Button_Click_PointsClear(object sender, RoutedEventArgs e)
+        {
+            MyData.Points.Clear();
+        }
+
+        private void Button_Click_ChangeCanDragMove(object sender, RoutedEventArgs e)
+        {
+            MyData.IsCanDragMove = !MyData.IsCanDragMove;
+        }
+    }
+}
