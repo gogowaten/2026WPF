e722305658d3022a9b8490290e60f2ec3351666f
waten <gogowaten@yahoo.co.jp>
2026-04-08 22:33:15 +0900

    202604082233

---
+++ 20260408/CustomControl1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260408
+{
+    //public class DataThumb : Thumb
+    //{
+    //    static DataThumb()
+    //    {
+    //        DefaultStyleKeyProperty.OverrideMetadata(typeof(DataThumb), new FrameworkPropertyMetadata(typeof(DataThumb)));
+    //    }
+
+
+    //    public GeoLineData MyGeoData
+    //    {
+    //        get { return (GeoLineData)GetValue(MyGeoDataProperty); }
+    //        set { SetValue(MyGeoDataProperty, value); }
+    //    }
+    //    public static readonly DependencyProperty MyGeoDataProperty =
+    //        DependencyProperty.Register(nameof(MyGeoData), typeof(GeoLineData), typeof(DataThumb), new PropertyMetadata(null));
+
+
+
+    //    public DataThumb()
+    //    {
+    //        Loaded += (s, e) => { if (DataContext is GeoLineData data) { MyGeoData = data; } };
+    //    }
+
+    //}
+
+
+
+
+
+    public class GeoThumb : Thumb
+    {
+        static GeoThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GeoThumb), new FrameworkPropertyMetadata(typeof(GeoThumb)));
+        }
+
+        #region 依存関係プロパティ
+
+        public GeoLineData MyGeoData
+        {
+            get { return (GeoLineData)GetValue(MyGeoDataProperty); }
+            set { SetValue(MyGeoDataProperty, value); }
+        }
+        public static readonly DependencyProperty MyGeoDataProperty =
+            DependencyProperty.Register(nameof(MyGeoData), typeof(GeoLineData), typeof(GeoThumb), new PropertyMetadata(null));
+
+
+
+        // ドラッグ移動切り替え
+        public bool IsCanDragMove
+        {
+            get { return (bool)GetValue(IsCanDragMoveProperty); }
+            set { SetValue(IsCanDragMoveProperty, value); }
+        }
+        public static readonly DependencyProperty IsCanDragMoveProperty =
+            DependencyProperty.Register(nameof(IsCanDragMove), typeof(bool), typeof(GeoThumb), new FrameworkPropertyMetadata(false, OnMyIsDragMoveChanged));
+
+        // DragDeltaの付け外し
+        private static void OnMyIsDragMoveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GeoThumb thumb)
+            {
+                if (e.NewValue is bool isMove && isMove)
+                {
+                    if (double.IsNaN(Canvas.GetLeft(thumb)))
+                    {
+                        Canvas.SetLeft(thumb, 0);
+                        Canvas.SetTop(thumb, 0);
+                    }
+                    thumb.DragDelta += Thumb_DragDelta;
+                    thumb.DragCompleted += Thumb_DragCompleted;
+                }
+                else
+                {
+                    thumb.DragDelta -= Thumb_DragDelta;
+                    thumb.DragCompleted -= Thumb_DragCompleted;
+                }
+            }
+        }
+
+
+        #endregion 依存関係プロパティ
+
+        public GeoThumb()
+        {
+            Loaded += GeoThumb_Loaded;
+        }
+
+        private void GeoThumb_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is GeoLineData data)
+            {
+                MyGeoData = data;
+            }
+        }
+
+
+
+        #region ドラッグ移動
+
+
+        private static void Thumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            if (sender is GeoThumb thumb)
+            {
+                thumb.MyGeoData.InternalX += e.HorizontalChange;
+                thumb.MyGeoData.InternalY += e.VerticalChange;
+                e.Handled = true;
+            }
+
+        }
+
+        private static void Thumb_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            if (sender is GeoThumb thumb)
+            {
+                // 移動完了時、DataのWidthとheightを更新する
+                // この値はParentのThumbのサイズとバインドしている
+                var data = thumb.MyGeoData;
+                data.Width = data.BoundsWidth + data.InternalX;
+                data.Height = data.BoundsHeight + data.InternalY;
+
+                if (data.X > 0) { data.Width -= data.X; }
+                if (data.Y > 0) { data.Height -= data.Y; }
+
+
+                // 移動後座標がマイナスになったときは、0にしたいので、
+                // その分ParentThumbを逆に移動させる
+                if (data.InternalX < 0)
+                {
+                    data.Width = data.BoundsWidth; // ParentThumbのサイズは図形と同じになるはず
+                    data.X += data.InternalX; // ParentThumbを逆側に移動させてから
+                    data.InternalX = 0; // 自身の座標
+                                        //data.X = 0; // ParentThumbがマイナス座標になったときの処理はRootで行うはず
+                }
+                if (data.InternalY < 0)
+                {
+                    data.Height = data.BoundsHeight;
+                    data.Y += data.InternalY;
+                    data.InternalY = 0;
+                }
+            }
+        }
+
+        #endregion ドラッグ移動
+
+
+
+    }
+
+
+}
---
+++ 20260408/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260408
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GeoLineData MyData { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyData = CreateData();
+            DataContext = this;
+        }
+
+        private GeoLineData CreateData()
+        {
+            GeoLineData data = new()
+            {
+                Points = [new Point(-20, 0), new Point(-20, 100)],
+                //Points = [new Point(80, 0), new Point(80, 100)],
+                //Points = [new Point(20, 30), new Point(120, 10), new Point(10, 100)],
+                Stroke = new SolidColorBrush(Color.FromArgb(100, 200, 0, 0)),
+                StrokeThickness = 100,
+                Background = new SolidColorBrush(Color.FromArgb(100, 200, 0, 0)),// Brushes.Gainsboro,
+                IsCanDragMove = true,
+                IsOffset = true,
+                MiterLimit = 10,
+                Name = "テスト用"
+            };
+            return data;
+        }
+
+        private void Button_Click_Check(object sender, RoutedEventArgs e)
+        {
+            //var neko = MyElement.DataContext;
+        }
+
+        private void Button_Click_ChangeDragMove(object sender, RoutedEventArgs e)
+        {
+            //MyElement.ChangeGeoLineDragMove();
+            MyData.IsCanDragMove = !MyData.IsCanDragMove;
+        }
+
+        private void Button_Click_Thickness10(object sender, RoutedEventArgs e)
+        {
+            MyData.StrokeThickness = 100;
+        }
+    }
+}
