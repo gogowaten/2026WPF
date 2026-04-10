4e29611682625266496088a118c5ef67bec82471
waten <gogowaten@yahoo.co.jp>
2026-04-10 22:26:01 +0900

    202604102225

---
+++ 20260410_GeoLine_GeoThumb/GeoLine.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace _20260410_GeoLine_GeoThumb
+{
+    public class GeoLine : Shape
+    {
+        private Geometry? _cachedGeometry;
+
+        protected override Geometry DefiningGeometry
+        {
+            get
+            {
+                // キャッシュが在ればそれを返して終わる
+                if (_cachedGeometry is not null)
+                {
+                    return _cachedGeometry;
+                }
+
+                if (MyPoints is null || MyPoints.Count < 2)
+                {
+                    _cachedGeometry = null;
+                    MyGeometryBounds = MyData.OnUpdateBounds(_cachedGeometry);
+                    return Geometry.Empty;
+                }
+
+                PathGeometry geo = MakeLineGeometry(MyPoints);
+                _cachedGeometry = geo;
+                MyGeometryBounds = MyData.OnUpdateBounds(_cachedGeometry);
+                return geo;
+            }
+        }
+
+        #region 依存関係プロパティ
+
+        public GeoLineData MyData
+        {
+            get { return (GeoLineData)GetValue(MyDataProperty); }
+            set { SetValue(MyDataProperty, value); }
+        }
+        public static readonly DependencyProperty MyDataProperty =
+            DependencyProperty.Register(nameof(MyData), typeof(GeoLineData), typeof(GeoLine), new PropertyMetadata(null));
+
+
+        public Rect MyGeometryBounds
+        {
+            get { return (Rect)GetValue(MyGeometryBoundsProperty); }
+            set { SetValue(MyGeometryBoundsProperty, value); }
+        }
+        public static readonly DependencyProperty MyGeometryBoundsProperty =
+            DependencyProperty.Register(nameof(MyGeometryBounds), typeof(Rect), typeof(GeoLine), new PropertyMetadata(null));
+
+        public bool MyIsOffset
+        {
+            get { return (bool)GetValue(MyIsOffsetProperty); }
+            set { SetValue(MyIsOffsetProperty, value); }
+        }
+        public static readonly DependencyProperty MyIsOffsetProperty =
+            DependencyProperty.Register(nameof(MyIsOffset), typeof(bool), typeof(GeoLine), new PropertyMetadata(false, OnMyIsOffsetChanged));
+        private static void OnMyIsOffsetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GeoLine geo)
+            {
+                geo.InvalidateVisual(); // 再描画
+            }
+        }
+
+        public Pen MyStrokePen
+        {
+            get { return (Pen)GetValue(MyStrokePenProperty); }
+            set { SetValue(MyStrokePenProperty, value); }
+        }
+        public static readonly DependencyProperty MyStrokePenProperty =
+            DependencyProperty.Register(nameof(MyStrokePen), typeof(Pen), typeof(GeoLine), new PropertyMetadata(null, OnMyStrokePenChanged));
+
+        private static void OnMyStrokePenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // pen変更時の処理
+            if (d is GeoLine geo)
+            {
+                //geo.UpdateMySize(); // 必須、描画更新も兼ねている
+                geo.MyGeometryBounds = geo.MyData.OnUpdateBounds(geo._cachedGeometry);
+            }
+        }
+
+
+        /// <summary>
+        /// Pointsの型はPointCollectionでもObservableCollectionのどちらでも同じ結果で
+        /// ObservableでPointの追加をしても通知はされないのは、依存関係プロパティでのCollectionは
+        /// Collection自体が変化したときだけ通知される仕様、なのでPoint追加で通知したいときは
+        /// コールバックの中からCollectionのChangedイベントを購読するようにすればいい
+        /// コールバックはPoint追加では呼ばれないけど、起動時には必ず呼ばれるから
+        /// </summary>
+        public PointCollection MyPoints
+        {
+            get { return (PointCollection)GetValue(MyPointsProperty); }
+            set { SetValue(MyPointsProperty, value); }
+        }
+        // AffectsMeasure必須
+        public static readonly DependencyProperty MyPointsProperty =
+            DependencyProperty.Register(nameof(MyPoints), typeof(PointCollection), typeof(GeoLine), new FrameworkPropertyMetadata(null, OnPointCollectionChanged));
+
+        private static void OnPointCollectionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is GeoLine geo)
+            {
+                // 古いCollectionのイベント購読を解除(メモリリーク防止)
+                if (e.OldValue is PointCollection oldCollection)
+                {
+                    oldCollection.Changed -= geo.PointCollection_Changed;
+                }
+
+                // 新しいCollectionのイベントを購読を開始
+                if (e.NewValue is PointCollection newCollection)
+                {
+                    newCollection.Changed += geo.PointCollection_Changed;
+                }
+
+            }
+        }
+
+        private void PointCollection_Changed(object? sender, EventArgs e)
+        {
+            // キャッシュクリアしてから再描画
+            _cachedGeometry = null;
+            InvalidateMeasure(); // ほぼ完璧、図形のActualが更新されないけど、使わないので問題ない
+            //InvalidateVisual(); // これでは不足、サイズが更新されない
+            //InvalidateArrange(); // 全く足りない、図形自体すら再描画されない
+            //UpdateLayout(); // 全く足りない、図形自体すら再描画されない
+            //InvalidateMeasure();
+
+        }
+
+        #endregion 依存関係プロパティ
+
+
+        #region コンストラクタ
+        public GeoLine()
+        {
+            //SetMyBind();
+            MyData = new();
+            Loaded += GeoLine_Loaded;
+
+        }
+
+
+
+        private void GeoLine_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is GeoLineData data)
+            {
+                MyData = data;
+                MyGeometryBounds = data.OnUpdateBounds(_cachedGeometry);
+                InvalidateVisual();
+            }
+        }
+
+
+
+        #endregion コンストラクタ
+
+        #region オーバーライド
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            // オフセット表示の場合はTranslateTransformで変形したものを描画
+            if (MyIsOffset)
+            {
+                //drawingContext.PushTransform(new TranslateTransform(-MyData.Bounds.Left, -MyData.Bounds.Top));
+                drawingContext.PushTransform(new TranslateTransform(-MyGeometryBounds.Left, -MyGeometryBounds.Top));
+            }
+            if (MyData is GeoLineData data && data.Background is not null)
+            {
+                drawingContext.DrawRectangle(data.Background, null, new Rect(MyData.BoundsLeft, MyData.BoundsTop, MyData.BoundsWidth, MyData.BoundsHeight));
+            }
+            base.OnRender(drawingContext);
+        }
+        #endregion オーバーライド
+
+
+        #region privateメソッド
+
+        private static PathGeometry MakeLineGeometry(IEnumerable<Point> pc)
+        {
+            if (!pc.Any()) { return new PathGeometry(); }
+
+            var seg = new PolyLineSegment(pc, true);
+            var fig = new PathFigure(pc.First(), [seg], false);
+            var geo = new PathGeometry([fig]);
+            return geo;
+        }
+
+
+        #endregion privateメソッド
+
+        #region publicメソッド
+
+
+        public Size GetBoundsSize()
+        {
+            return MyGeometryBounds.Size;
+        }
+        #endregion publicメソッド
+    }
+
+
+
+
+
+
+}
---
+++ 20260410_GeoLine_GeoThumb/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260410_GeoLine_GeoThumb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public GeoLineData MyData { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyData = CreateData();
+            DataContext = this;
+        }
+
+        private GeoLineData CreateData()
+        {
+            var data = new GeoLineData()
+            {
+                MyPoints = [new Point(80, 0), new Point(80, 100)],
+                Stroke = Brushes.LimeGreen,
+                StrokeThickness = 20,
+                Background = Brushes.CadetBlue,
+                Fill = Brushes.Silver,
+                IsOffset = true,
+                IsCanDragMove = true,
+            };
+            return data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MyData?.MyPoints?.Add(new Point(0, 10));
+            MyData?.MyPoints?.Add(new Point(100, 30));
+        }
+
+        private void Button_Click_Clear(object sender, RoutedEventArgs e)
+        {
+            MyData.MyPoints?.Clear();
+        }
+
+        private void Button_Click_ChangePoint(object sender, RoutedEventArgs e)
+        {
+            MyData.MyPoints?[0] = new Point();
+        }
+
+        private void Button_Click_Add2(object sender, RoutedEventArgs e)
+        {
+            MyData.MyPoints?.Add(new Point(-40, -20));
+        }
+    }
+}
