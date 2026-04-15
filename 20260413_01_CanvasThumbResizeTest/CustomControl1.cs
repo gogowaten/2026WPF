409754514000e7387c69c43fdd2af27f90d1ab9e
waten <gogowaten@yahoo.co.jp>
2026-04-15 14:27:15 +0900

    202604151427

---
+++ 20260413_01_CanvasThumbResizeTest/CustomControl1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260413_01_CanvasThumbResizeTest
+{   /// <summary>
+    /// Thumb自体をリサイズした時TemplateのCanvasはどうなる？
+    /// まず、Thumbにサイズを指定する必要がある。もし無指定の場合は
+    /// ハンドルを移動させてもサイズはNaNから変化しないし、これはreSizeAdornerの仕様にしている
+    /// で、サイズ指定してからハンドル移動するとリサイズされる
+    /// けど、中のCanvasはリサイズされないので、中央揃えの表示になる
+    /// </summary>
+    public class CanvasThumb2 : Thumb
+    {
+        static CanvasThumb2()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CanvasThumb2), new FrameworkPropertyMetadata(typeof(CanvasThumb2)));
+        }
+        public CanvasThumb2()
+        {
+
+        }
+    }
+
+
+
+
+
+
+    public class CanvasThumb : Thumb
+    {
+
+        public Canvas MyTemplateCanvas
+        {
+            get { return (Canvas)GetValue(MyTemplateCanvasProperty); }
+            set { SetValue(MyTemplateCanvasProperty, value); }
+        }
+        public static readonly DependencyProperty MyTemplateCanvasProperty =
+            DependencyProperty.Register(nameof(MyTemplateCanvas), typeof(Canvas), typeof(CanvasThumb), new PropertyMetadata(null));
+
+        static CanvasThumb()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CanvasThumb), new FrameworkPropertyMetadata(typeof(CanvasThumb)));
+        }
+
+        public CanvasThumb()
+        {
+
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if (GetTemplateChild("PART_Panel") is Canvas panel)
+            {
+                MyTemplateCanvas = panel;
+            }
+        }
+
+        public void AddResizeAdorner()
+        {
+            ResizeAdorner.AddResizeAdorner(MyTemplateCanvas);
+        }
+
+        public void RemoveResizeAdorner() => ResizeAdorner.RemoveResizeAdorner(MyTemplateCanvas);
+    }
+
+
+    public class CustomControl1 : Control
+    {
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
---
+++ 20260413_01_CanvasThumbResizeTest/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20260413_01_CanvasThumbResizeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            MyCanvasThumb.AddResizeAdorner();
+            ResizeAdorner.AddResizeAdorner(MyCanvasThumb2);
+        }
+        private void Button_Click_Remove(object sender, RoutedEventArgs e)
+        {
+            MyCanvasThumb.RemoveResizeAdorner();
+            _ = ResizeAdorner.RemoveResizeAdorner(MyCanvasThumb2);
+        }
+
+    }
+}
