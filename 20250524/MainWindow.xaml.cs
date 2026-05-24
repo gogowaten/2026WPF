a39f2a0583307252b1349bf8a6addf51f9ef7828
waten <gogowaten@yahoo.co.jp>
2026-05-25 03:29:02 +0900

    202605250328

--- 20250524/MainWindow.xaml.cs
+++ 20250524/MainWindow.xaml.cs
@@ -109,22 +109,6 @@ namespace _20250524
             }
         }
 
-        private void ButtonChangeStretchNone_Click(object sender, RoutedEventArgs e)
-        {
-            ImageControl.Stretch = Stretch.None;
-        }
-        private void ButtonChangeStretchUniform_Click(object sender, RoutedEventArgs e)
-        {
-            ImageControl.Stretch = Stretch.Uniform;
-        }
-        private void ButtonChangeStretchFill_Click(object sender, RoutedEventArgs e)
-        {
-            ImageControl.Stretch = Stretch.Fill;
-        }
-        private void ButtonChangeStretchUniformToFill_Click(object sender, RoutedEventArgs e)
-        {
-            ImageControl.Stretch = Stretch.UniformToFill;
-        }
 
 
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
@@ -149,7 +133,7 @@ namespace _20250524
             }
         }
 
-      
+
 
         private void ButtonSetScale_Click(object sender, RoutedEventArgs e)
         {
@@ -181,9 +165,78 @@ namespace _20250524
             return result;
         }
 
+        // 試験版：要素からBitmap作成、LayoutTransformによる回転拡大対応
+        public static RenderTargetBitmap MakeBitmapFromElement2(FrameworkElement item, FrameworkElement parent)
+        {
+            double dpi = 96.0 * PresentationSource.FromVisual(item).CompositionTarget.TransformFromDevice.M11;
+
+            Rect bounds = new(0, 0, item.ActualWidth, item.ActualHeight);
+            Rect itemBounds = VisualTreeHelper.GetContentBounds(item); // ActualSizeと同じ
+            GeneralTransform TFV = item.TransformToVisual(parent); 
+            GeneralTransform TFA = item.TransformToAncestor(parent); // TFVと同じ、Parentのみ対応
+            GeneralTransform TFD = parent.TransformToDescendant(item); // これは違う
+            Transform LT = item.LayoutTransform; // これで十分
+            var tfvBounds = TFV.TransformBounds(bounds);
+            var tfaBounds = TFA.TransformBounds(bounds);
+            var tfdBounds = TFD.TransformBounds(bounds);
+            var ltBounds = item.LayoutTransform.TransformBounds(bounds);
+
+            // オフセットする
+            DrawingVisual dv = new();
+            dv.Offset = new Vector(-ltBounds.X, -ltBounds.Y);
+
+            using (DrawingContext context = dv.RenderOpen())
+            {
+
+                //VisualBrush brush = new(item);
+                //context.DrawRectangle(brush, null, tfvBounds);
+                //context.DrawRectangle(new VisualBrush(item), null, tfvBounds);
+
+                // これがいい
+                context.DrawRectangle(new VisualBrush(item), null, ltBounds);
+
+                // BitmapCacheBrush、これだとぼやけるし、変なところで上下がループした画像になる
+                //context.DrawRectangle(new BitmapCacheBrush(item), null, ltBounds);
+            }
+            //RenderTargetBitmap bmp = new((int)tfvBounds.Width, (int)tfvBounds.Height, dpi, dpi, PixelFormats.Pbgra32);
+            RenderTargetBitmap bmp =
+                new(MyCeiling(ltBounds.Width), MyCeiling(ltBounds.Height), dpi, dpi, PixelFormats.Pbgra32);
+            bmp.Render(dv);
+            return bmp;
+        }
+
+        // 完成版：要素からBitmap作成、LayoutTransformによる回転拡大対応
+        public static RenderTargetBitmap MakeBitmapFromElement3(FrameworkElement item)
+        {
+            double dpi = 96.0 * PresentationSource.FromVisual(item).CompositionTarget.TransformFromDevice.M11;
+            Rect bounds = new(0, 0, item.ActualWidth, item.ActualHeight); // 元のBounds
+            var ltBounds = item.LayoutTransform.TransformBounds(bounds); // 変形後のBounds
+            DrawingVisual dv = new();
+            dv.Offset = new Vector(-ltBounds.X, -ltBounds.Y);
+
+            using (DrawingContext context = dv.RenderOpen())
+            {
+                VisualBrush brush = new(item) { Stretch = Stretch.None };
+                context.DrawRectangle(brush, null, ltBounds);
+                //context.DrawRectangle(new VisualBrush(item), null, ltBounds);
+            }
+            RenderTargetBitmap bmp =
+                new(MyCeiling(ltBounds.Width), MyCeiling(ltBounds.Height), dpi, dpi, PixelFormats.Pbgra32);
+            bmp.Render(dv);
+            return bmp;
+        }
+
+        public static int MyCeiling(double value)
+        {
+            return (int)Math.Ceiling(value);
+        }
+
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var bmp = MakeBitmapFromElement(ImageControl.ActualWidth, ImageControl.ActualHeight, ImageControl);
+            //var bmp = MakeBitmapFromElement2(ImageControl, this);
+            var bmp = MakeBitmapFromElement3(ImageControl);
             BitmapToPngImageToClipboard(bmp);
         }
     }
