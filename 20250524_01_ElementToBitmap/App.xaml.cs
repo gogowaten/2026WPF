d9864eb1537ebd2ac327fae00231653c38f40238
waten <gogowaten@yahoo.co.jp>
2026-05-25 12:30:23 +0900

    202605251230

--- 20250524_01_ElementToBitmap/App.xaml.cs
+++ 20250524_01_ElementToBitmap/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace _20250524_01_ElementToBitmap
+namespace _20260524_01_ElementToBitmap
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- 20250524_01_ElementToBitmap/MainWindow.xaml.cs
+++ 20250524_01_ElementToBitmap/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _20250524_01_ElementToBitmap
+namespace _20260524_01_ElementToBitmap
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
